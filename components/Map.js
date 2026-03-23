cf4e7228985a3eb73b67346f994ed9bc2871c700
vedranzaf <vedranzaf@users.noreply.github.com>
2026-03-23 10:28:13 +0100

    fix: switch map tiles from CARTO dark to CARTO light (Positron) to match clean design

--- components/Map.js
+++ components/Map.js
@@ -109,8 +109,8 @@ export default function Map({
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
 
         <MapClickHandler isPlacing={isPlacing} onMapClick={onMapClick} />
--- components/MiniMap.js
+++ components/MiniMap.js
@@ -18,7 +18,7 @@ export default function MiniMap({ lat, lng }) {
     >
       <TileLayer
         attribution='&copy; CARTO'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
       />
       <Marker position={[lat, lng]} />
     </MapContainer>
